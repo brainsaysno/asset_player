8f2fc652fc6948b27ca55a624315a4d66ed58234
Nicolás Russo <nicolas.russo.ortiz@gmail.com>
2023-04-03 00:26:50 -0300

    mvp

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { Button, StyleSheet, View } from "react-native";
+import { Audio } from "expo-av";
 
 export default function App() {
+  const audios = [
+    require("./assets/audios/1.mp3"),
+    require("./assets/audios/2.mp3"),
+  ];
+
+  const [sound, setSound] = useState();
+
+  const playAudio = async (audio) => {
+    console.log("Loading Sound");
+    const { sound } = await Audio.Sound.createAsync(audio);
+    setSound(sound);
+
+    console.log("Playing Sound");
+    await sound.playAsync();
+  };
+
+  useEffect(() => {
+    return sound
+      ? () => {
+        console.log("Unloading Sound");
+        sound.unloadAsync();
+      }
+      : undefined;
+  }, [sound]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {audios.map((audio, i) => (
+        <Button
+          title={`Audio #${i + 1}`}
+          onPress={(_) => playAudio(audio)}
+          key={i}
+        />
+      ))}
     </View>
   );
 }
@@ -13,8 +44,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
